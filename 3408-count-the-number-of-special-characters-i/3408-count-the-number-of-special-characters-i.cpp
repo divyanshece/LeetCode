257911034d740d01f0fa7f9f0294548f19571427
Divyansh Pandey <129187227+Div-Pandey@users.noreply.github.com>
2024-04-23 23:35:15 +0530

    Time: 0 ms (100%), Space: 8.9 MB (47.63%) - LeetHub

--- 3408-count-the-number-of-special-characters-i/3408-count-the-number-of-special-characters-i.cpp
+++ 3408-count-the-number-of-special-characters-i/3408-count-the-number-of-special-characters-i.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int numberOfSpecialChars(string s) {
-        set<int>s_1;
-        set<int>s_2;
+        set<int>s1;
+        set<int>s2;
         for(auto i:s) {
             if(i>='a' && i<='z')
-                s_1.insert(i-'a');
+                s1.insert(i-'a');
             else
-                s_2.insert(i-'A');
+                s2.insert(i-'A');
         }
         int c=0;
-        for(auto i:s_1) {
-            if(s_2.find(i)!=s_2.end())
+        for(auto i:s1) {
+            if(s2.find(i)!=s2.end())
                 c++;
         }
         return c;
