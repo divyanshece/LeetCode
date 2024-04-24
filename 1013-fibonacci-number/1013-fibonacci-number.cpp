fee0d614a412b03f7232a3110cbc0eeee9d76c3c
Divyansh Pandey <129187227+Div-Pandey@users.noreply.github.com>
2024-04-24 22:28:06 +0530

    Time: 15 ms (16.44%), Space: 7.1 MB (34.28%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n==0) {
-            return n;
-        }
-        if (n<=1) {
-            return n;
-        }
+        if (n<=1) return n;
         return fib(n-1) + fib(n-2);
     }
 };
