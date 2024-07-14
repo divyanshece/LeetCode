06ad095a700d1120329d5bad90ea00168e0a16f0
Divyansh Pandey <divyanshpandey242@gmail.com>
2024-07-14 11:38:16 +0530

    Time: 0 ms (100%), Space: 8.1 MB (83.33%) - LeetHub

--- 3484-lexicographically-smallest-string-after-a-swap/3484-lexicographically-smallest-string-after-a-swap.cpp
+++ 3484-lexicographically-smallest-string-after-a-swap/3484-lexicographically-smallest-string-after-a-swap.cpp
@@ -4,8 +4,8 @@ public:
         int n = s.length();
 
         for(int i=0;i<n-1;i++) {
-            int curr = s[i] - '0';
-            int next = s[i+1] - '0';
+            int curr = s[i] ;
+            int next = s[i+1];
 
             if (curr%2==0 && next%2==0 || curr%2 !=0 && next%2 !=0) {
                 if (curr>next) {
