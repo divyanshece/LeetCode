0dd2ffd24f35a925e9c921b53e2e1166736f7a33
Divyansh Pandey <divyanshpandey242@gmail.com>
2024-07-16 19:34:02 +0530

    Time: 105 ms (29.98%), Space: 75.6 MB (10.2%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,16 +1,13 @@
-class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, int>map;
-        for(int i=0;i<nums.size();i++) {
-            map[nums[i]]++;
-        }
-        for(int j=0;j<nums.size();j++) {
-            if(map[nums[j]]>=2) {
-                return true;
-            }
-        }
-        return false;
-        
-    }
-};
+// Contains Duplicate
+  class Solution {
+  public:
+      bool containsDuplicate(vector<int>& nums) {
+          unordered_map<int,int> mp;
+          for(auto i : nums) mp[i]++;
+          bool flag = false;
+          for(auto i : mp){
+              if(i.second >= 2) return true;
+          }
+          return flag;
+      }
+  };
