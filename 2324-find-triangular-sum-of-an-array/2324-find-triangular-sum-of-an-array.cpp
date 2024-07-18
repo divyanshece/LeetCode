6dff51a903cfc61ba9ced2230c946639e53ed84f
Divyansh Pandey <divyanshpandey242@gmail.com>
2024-07-18 12:16:47 +0530

    Time: 49 ms (98.47%), Space: 21.6 MB (51.21%) - LeetHub

--- 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     int triangularSum(vector<int>& nums) {
-        while(nums.size()>=1) {
-            if(nums.size()==1) {
-                return nums[0];
+        int n = nums.size();
+        for(int i=n-1; i>=1; i--) {
+            for(int j=0; j<i; j++) {
+                nums[j] = (nums[j] + nums[j+1])%10;
             }
-            int n = nums.size();
-            vector<int>newNums(n-1);
-            for(int i=0; i<newNums.size(); i++) {
-                newNums[i] = (nums[i] + nums[i+1]) % 10;
-            }
-            nums = newNums;
         }
         return nums[0];
         
-        
     }
 };
+
+
+// Brute force approach according to the flow of the question 
+// class Solution {
+// public:
+//     int triangularSum(vector<int>& nums) {
+//         while(nums.size()>=1) {
+//             if(nums.size()==1) {
+//                 return nums[0];
+//             }
+//             int n = nums.size();
+//             vector<int>newNums(n-1);
+//             for(int i=0; i<newNums.size(); i++) {
+//                 newNums[i] = (nums[i] + nums[i+1]) % 10;
+//             }
+//             nums = newNums;
+//         }
+//         return nums[0];
+        
+        
+//     }
+// };
