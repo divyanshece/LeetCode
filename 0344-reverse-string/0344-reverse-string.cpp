3680ce85b93395f55aa6feaedc9a6e3bcf29d04b
Divyansh Pandey <divyanshpandey242@gmail.com>
2024-08-19 19:52:11 +0530

    Time: 17 ms (56.95%), Space: 27.2 MB (56.24%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -3,10 +3,8 @@ public:
     void reverseString(vector<char>& s) {
         int i = 0;
         int j = s.size()-1;
-        while (i<=j) {
-            swap(s[i], s[j]);
-            i++;
-            j--;
+        while (i<j) {
+            swap(s[i++], s[j--]);
         }
         
     }
