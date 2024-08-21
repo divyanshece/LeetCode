792761e435d0cd08b6e3849a7bb9bf18e53fdeae
Divyansh Pandey <divyanshpandey242@gmail.com>
2024-08-21 20:57:48 +0530

    Time: 0 ms (100%), Space: 7.7 MB (45.62%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.cpp
+++ 0742-to-lower-case/0742-to-lower-case.cpp
@@ -1,10 +1,24 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        for (int i =0; i<s.size(); i++) {
-            s[i] = tolower(s[i]);
-        }
-        return s;
-        
+        for(int i=0; i<s.size(); i++) {
+            if(s[i] <='Z' && s[i]>='A') {
+                s[i] = s[i]-'A'+'a';
+            }
+        } 
+        return s; 
     }
 };
+
+
+// Using inbuilt funtion ---
+// class Solution {
+// public:
+//     string toLowerCase(string s) {
+//         for (int i =0; i<s.size(); i++) {
+//             s[i] = tolower(s[i]);
+//         }
+//         return s;
+        
+//     }
+// };
