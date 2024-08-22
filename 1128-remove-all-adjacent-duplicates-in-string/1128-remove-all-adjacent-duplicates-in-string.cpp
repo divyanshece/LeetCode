a0a309797b906dc9612d5db2bbcfc22a48614488
Divyansh Pandey <divyanshpandey242@gmail.com>
2024-08-22 18:30:12 +0530

    Time: 17 ms (67.49%), Space: 12.6 MB (43.32%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        int i = 0;
-        while (i<s.length()-1) {
-            if(s[i] == s[i+1]) {
-                s.erase(i, 2);
-                i = 0;
-                if (s.length()==0) {
-                    return s;
-                }
+        string temp = "";
+        temp.push_back(s[0]);
+        int j = 1;
+        for(int i=1; i<s.length(); i++) {
+            if(temp.length() > 0 && temp[temp.length()-1] == s[i]) {
+                temp.pop_back();
             }
             else {
-                i++;
+               temp.push_back(s[i]);
             }
         }
-        return s;
+        return temp;
     }
 };
+//Very large time complexity -- Brute Force 
+// class Solution {
+// public:
+//     string removeDuplicates(string s) {
+//         int i = 0;
+//         while (i<s.length()-1) {
+//             if(s[i] == s[i+1]) {
+//                 s.erase(i, 2);
+//                 i = 0;
+//                 if (s.length()==0) {
+//                     return s;
+//                 }
+//             }
+//             else {
+//                 i++;
+//             }
+//         }
+//         return s;
+//     }
+// };
