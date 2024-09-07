5b79f472bd40b160efb1e28bbcf836847db39afe
Divyansh Pandey <divyanshpandey242@gmail.com>
2024-09-07 13:20:14 +0530

    Time: 73 ms (78.24%), Space: 92.7 MB (24.99%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
-        // Check if the original array can be exactly filled into an m x n matrix
-        if (original.size() != m * n) {
-            return {}; // Return an empty 2D array
-        }
-        
-        // Initialize the 2D array
-        vector<vector<int>> ans(m, vector<int>(n, 0));
-        
+        vector<vector<int>>ans(m, vector<int>(n, 0));
         int i = 0;
-        for (int row = 0; row < m; ++row) {
-            for (int column = 0; column < n; ++column) {
-                ans[row][column] = original[i++];
-            }
+        int row = 0;
+        if (original.size() != m * n) {
+            return {}; 
         }
-        
+        while(i<original.size() && row < m) {
+            int column = 0;
+            
+            while(column < n) {
+                ans[row][column] = original[i];
+                column++;
+                i++;
+            }
+            row++;
+        }
         return ans;
     }
 };
