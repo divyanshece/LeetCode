45f76769628988d33be76bd4d4be3f05b3fc8e4b
Divyansh Pandey <divyanshpandey242@gmail.com>
2024-09-08 23:12:56 +0530

    Time: 6 ms (18.02%), Space: 10.1 MB (29.43%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int getsum(string s) {
+        int ans = 0;
+        for(auto c: s) {
+            ans = ans + (c - '0');
+        }
+
+        return ans;
+    }
+
+    int getLucky(string s, int k) {
+        string ans;
+        int result;
+
+        for(auto c: s) {
+            int temp =  (c - 'a' + 1);
+            ans += std::to_string(temp);
+        }
+
+        
+        while(k-- > 0) {
+            int sum = getsum(ans); 
+            ans = std::to_string(sum);
+        }
+        
+
+        return std::stoi(ans);
+    }
+};
